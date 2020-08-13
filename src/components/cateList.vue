3b1e4156b02ab2ef1fde5563e8d72cc8d715479a
2218675712 <2218675712@qq.com>
2020-08-13 21:48:37 +0800

    2020年08月13日21:48:30 分类商品接口

--- src/components/cateList.vue
+++ src/components/cateList.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="cateList">
+<!--    侧边栏-->
     <div class="sidebarClass">
-      <van-sidebar v-model="sidebarKey">
+      <van-sidebar v-model="sidebarKey" @change="onChangeSidebar">
         <van-sidebar-item v-for="(item,index) in sidebar_arr" :key="index" :title="item.txt"/>
       </van-sidebar>
+    </div>
+<!--    右侧商品区-->
+    <div class="sidebarGoods">
+      <div class="goodsItem" v-for="(n,index) in goodsListObj" :key="index">
+        <img :src="n.goods_img" alt="">
+        <p>{{n.goods_name}}</p>
+        <b>{{n.goods_price}}</b>
+      </div>
     </div>
     <!-- 底部组件-->
     <Footer_bar></Footer_bar>
@@ -27,7 +36,7 @@ export default {
           txt: '爆品专区'
         }, {
           txt: '新品专区'
-        }, {
+        }/*, {
           txt: '居家生活'
         }, {
           txt: '服饰鞋包'
@@ -43,8 +52,24 @@ export default {
           txt: '数码家电'
         }, {
           txt: '严选全球'
-        }
-      ]
+        }*/
+      ],
+      goodsListObj: []
+    }
+  },created() {
+    this.axios.get('cate_goods_list_1').then(_d => {
+      this.goodsListObj = _d.data
+    })
+  },
+  methods: {
+    /**
+     * 获取分类列表商品
+     * @param _inx
+     */
+    onChangeSidebar(_inx) {
+      this.axios.get('cate_goods_list_' + _inx).then(_d => {
+        this.goodsListObj = _d.data
+      })
     }
   }
 }
@@ -52,19 +77,30 @@ export default {
 
 <style scoped lang="less">
 .cateList {
+  position: relative;
+  padding-left: 2.16rem;
   .sidebarClass {
+    position: fixed;
+    left: 0;
+    top: 0;
     .van-sidebar {
       width: 2.16rem;
 
-      .van-sidebar-item__text {
-        font-size: .37333rem;
-        line-height: .66667rem;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        color: #333;
-      }
     }
 
+  }
+  .sidebarGoods{
+    //position: relative;
+    .goodsItem{
+      float: left;
+      width: 44%;
+      margin: 0rem 0.2rem 0.44rem 0.2rem;
+      font-size: .37333rem;
+      overflow: hidden;
+      img{
+        height: 4.6rem;
+      }
+    }
   }
 }
 
